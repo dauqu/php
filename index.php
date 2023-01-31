1c0deeaabb6d9cbc3bb9d176bad78e70c015d7b0
Dauqu <95732637+dauqu@users.noreply.github.com>
2023-01-31 19:56:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php
-phpinfo();
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <iframe id='fp_embed_player'
+        src='https://livetvline.com:8888/embed_player?urlServer=wss://livetvline.com:8443&streamName=Eng_test111&mediaProviders=WebRTC,Flash,MSE,WSPlayer'
+        marginwidth='0' marginheight='0' frameborder='0' width='700px' height='400px' scrolling='no'
+        allowfullscreen='allowfullscreen'></iframe>
+</body>
+
+</html>
